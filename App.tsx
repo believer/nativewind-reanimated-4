b7f150855e705b7dbf2868f027bcd898a0588bf2
Rickard Natt och Dag <rickard.natt-och-dag@seb.se>
2025-09-03 10:48:58 +0200

    chore: setup for nativewind and reanimated 4

--- App.tsx
+++ App.tsx
@@ -1,45 +1,75 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import "./global.css";
+import React from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+import type { SharedValue } from "react-native-reanimated";
+import Animated, {
+	scrollTo,
+	useAnimatedRef,
+	useDerivedValue,
+	useScrollOffset,
+	useSharedValue,
+} from "react-native-reanimated";
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+const ITEM_COUNT = 10;
+const ITEM_SIZE = 100;
+const ITEM_MARGIN = 10;
 
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+export default function App() {
+	const ref = useAnimatedRef<Animated.ScrollView>();
+	const scroll = useSharedValue(0);
 
-  return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
+	useDerivedValue(() => {
+		scrollTo(ref, scroll.get() * (ITEM_SIZE + 2 * ITEM_MARGIN), 0, true);
+	});
+
+	const items = Array.from(Array(ITEM_COUNT).keys());
+
+	return (
+		<View className="flex-1 items-center">
+			<Incrementor increment={-1} scroll={scroll} />
+			<View className="w-full h-[250px] items-center">
+				<Animated.ScrollView ref={ref}>
+					{items.map((_, i) => (
+						<View
+							key={i}
+							className="bg-[red] items-center justify-center"
+							style={{
+								width: ITEM_SIZE,
+								height: ITEM_SIZE,
+								margin: ITEM_MARGIN,
+							}}
+						>
+							<Text style={{ textAlign: "center" }}>{i}</Text>
+						</View>
+					))}
+				</Animated.ScrollView>
+			</View>
+			<Incrementor increment={1} scroll={scroll} />
+		</View>
+	);
 }
 
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
+const Incrementor = ({
+	increment,
+	scroll,
+}: {
+	increment: number;
+	scroll: SharedValue<number>;
+}) => (
+	<View className="flex-1 items-center justify-center">
+		<Button
+			onPress={() => {
+				scroll.set(
+					scroll.get() + increment > 0
+						? scroll.get() + increment
+						: ITEM_COUNT - 1 + increment,
+				);
 
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
-
-export default App;
+				if (scroll.get() >= ITEM_COUNT - 2) {
+					scroll.set(0);
+				}
+			}}
+			title={`Scroll ${Math.abs(increment)} ${increment > 0 ? "down" : "up"}`}
+		/>
+	</View>
+);
